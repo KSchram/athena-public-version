9b9d15befcc800d2849dee97653c39f04d470b5c
Katie Schram <KatieSchram@Marks-MacBook-Air.local>
2018-07-04 23:58:46 -0400

    Fixed problem generator file and input file.

--- src/pgen/taylor_green.cpp
+++ src/pgen/taylor_green.cpp
@@ -2,14 +2,13 @@
 // Athena++ astrophysical MHD code
 // Copyright(C) 2014 James M. Stone <jmstone@princeton.edu> and other code contributors
 // Licensed under the 3-clause BSD License, see LICENSE file for details
-
+//
 // Edited by Katie Schram 2018
 //========================================================================================
 //! \file taylor_green.cpp
 //  \brief Problem generator for Taylor-Green vortex problem.
 //
-// REFERENCE: For example, see: G. Toth,  "The div(B)=0 constraint in shock capturing
-//   MHD codes", JCP, 161, 605 (2000)
+// REFERENCE:
 //========================================================================================
 
 // C/C++ headers
@@ -29,10 +28,6 @@
 #include "../hydro/hydro.hpp"
 #include "../mesh/mesh.hpp"
 
-#if !MAGNETIC_FIELDS_ENABLED
-#error "This problem generator requires magnetic fields"
-#endif
-
 
 //========================================================================================
 //! \fn void MeshBlock::ProblemGenerator(ParameterInput *pin)
@@ -42,64 +37,30 @@
 //========================================================================================
 
 void MeshBlock::ProblemGenerator(ParameterInput *pin) {
-  Real gm1 = peos->GetGamma() - 1.0;
+  Real v0 = pin->GetReal("problem","v0");
+  Real d0 = pin->GetReal("problem","d0");
+  Real p0 = pin->GetReal("problem","p0");
+  Real L = pin->GetReal("problem","L");
 
   AthenaArray<Real> az;
   int nx1 = (ie-is)+1 + 2*(NGHOST);
   int nx2 = (je-js)+1 + 2*(NGHOST);
-  az.NewAthenaArray(nx2,nx1);
+  int nx3 = (ke-ks)+1 + 2*(NGHOST);
+  az.NewAthenaArray(nx3,nx2,nx1);
 
-  Real B0 = 1.0/std::sqrt(4.0*PI);
-  Real d0 = 25.0/(36.0*PI);
-  Real v0 = 1.0;
-  Real p0 = 5.0/(12.0*PI);
-
-  // Initialize vector potential
-  for (int j=js; j<=je+1; ++j) {
-  for (int i=is; i<=ie+1; ++i) {
-    az(j,i) = B0/(4.0*PI)*(cos(4.0*PI*pcoord->x1f(i)) - 2.0*cos(2.0*PI*pcoord->x2f(j)));
-  }}
-
-  // Initialize density, momentum, face-centered fields
+  Real R = 8.314;
+  Real T0 = p0/(d0*R);
+    
+  // Initialize density and momentum
   for (int k=ks; k<=ke; k++) {
   for (int j=js; j<=je; j++) {
   for (int i=is; i<=ie; i++) {
-    phydro->u(IDN,k,j,i) = d0; //density
-    phydro->u(IM1,k,j,i) = -d0*v0*cos(pcoord->x1v(i))*sin(pcoord->x2v(j)); //y momentum, L = 1
-    phydro->u(IM2,k,j,i) = d0*v0*sin(pcoord->x1v(i))*cos(pcoord->x2v(j)); //x momentum, L = 1
+    Real p = p0 + (d0*v0*v0/16)*(cos(2*pcoord->x1v(i)/L)+cos(2*pcoord->x2v(j)/L))*(cos(2*pcoord->x3v(k)/L)+2);
+    phydro->u(IDN,k,j,i) = p/(R*T0); //density
+    phydro->u(IM1,k,j,i) = -d0*v0*cos(pcoord->x1v(i)/L)*sin(pcoord->x2v(j)/L)*cos(pcoord->x3v(k)/L); //y momentum, L = 2pi
+    phydro->u(IM2,k,j,i) = d0*v0*sin(pcoord->x1v(i)/(2.0*PI))*cos(pcoord->x2v(j)/(2.0*PI))*cos(pcoord->x3v(k)/L); //x momentum, L = 2pi
     phydro->u(IM3,k,j,i) = 0.0; //z momentum
   }}}
-
-  // initialize interface B
-  for (int k=ks; k<=ke; k++) {
-  for (int j=js; j<=je; j++) {
-  for (int i=is; i<=ie+1; i++) {
-    pfield->b.x1f(k,j,i) = (az(j+1,i) - az(j,i))/pcoord->dx2f(j);
-  }}}
-  for (int k=ks; k<=ke; k++) {
-  for (int j=js; j<=je+1; j++) {
-  for (int i=is; i<=ie; i++) {
-    pfield->b.x2f(k,j,i) = (az(j,i) - az(j,i+1))/pcoord->dx1f(i);
-  }}}
-  for (int k=ks; k<=ke+1; k++) {
-  for (int j=js; j<=je; j++) {
-  for (int i=is; i<=ie; i++) {
-    pfield->b.x3f(k,j,i) = 0.0;
-  }}}
-
-  // initialize total energy
-  if (NON_BAROTROPIC_EOS) {
-    for (int k=ks; k<=ke; k++) {
-    for (int j=js; j<=je; j++) {
-    for (int i=is; i<=ie; i++) {
-      phydro->u(IEN,k,j,i) = p0/gm1 +
-          0.5*(SQR(0.5*(pfield->b.x1f(k,j,i) + pfield->b.x1f(k,j,i+1))) +
-               SQR(0.5*(pfield->b.x2f(k,j,i) + pfield->b.x2f(k,j+1,i))) +
-               SQR(0.5*(pfield->b.x3f(k,j,i) + pfield->b.x3f(k+1,j,i)))) + (0.5)*
-          (SQR(phydro->u(IM1,k,j,i)) + SQR(phydro->u(IM2,k,j,i))
-           + SQR(phydro->u(IM3,k,j,i)))/phydro->u(IDN,k,j,i);
-    }}}
-  }
 
   az.DeleteAthenaArray();
   return;
