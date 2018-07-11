22ede1f4652390021f8f1fb3ff52e2195df75556
Katie Schram <KatieSchram@Marks-MacBook-Air.local>
2018-07-10 20:49:08 -0400

    minor changes

--- src/pgen/taylor_green.cpp
+++ src/pgen/taylor_green.cpp
@@ -40,8 +40,11 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
   Real M0 = pin->GetReal("problem","M0");
   Real rho0 = pin->GetReal("problem","rho0");
   Real p0 = pin->GetReal("problem","p0");
-  Real x_len = pin->GetReal("mesh","x1max") - pin->GetReal("mesh","x1min");
+  Real L = pin->GetReal("mesh","x1max")/PI;
   Real gamma = pin->GetReal("hydro","gamma");
+    
+  Real c = sqrt(gamma*p0/rho0);
+  Real v0 = c*M0;
     
   AthenaArray<Real> az;
   int nx1 = (ie-is)+1 + 2*(NGHOST);
@@ -49,24 +52,25 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
   int nx3 = (ke-ks)+1 + 2*(NGHOST);
   az.NewAthenaArray(nx3,nx2,nx1);
 
-  Real L = 2.0*PI*x_len;
-    
   // Initialize density and momentum
   for (int k=ks; k<=ke; k++) {
   for (int j=js; j<=je; j++) {
   for (int i=is; i<=ie; i++) {
-    Real c = sqrt(gamma*p0/rho0);
-    Real v0 = c*M0;
-    Real p = p0 + (rho0*v0*v0/16)*(cos(2*pcoord->x1v(i)/L)+cos(2*pcoord->x2v(j)/L))*(cos(2*pcoord->x3v(k)/L)+2);
+
+    Real p = p0 + (rho0*v0*v0/16)*(cos(2*i/L)+cos(2*j/L))*(cos(2*k/L)+2);
     Real rho = p*rho0/p0;
     
     phydro->u(IDN,k,j,i) = rho; //density
-    phydro->u(IM1,k,j,i) = -rho0*v0*cos(pcoord->x1v(i)/L)*sin(pcoord->x2v(j)/L)*cos(pcoord->x3v(k)/L); //y momentum, L = 2pi
-    phydro->u(IM2,k,j,i) = rho0*v0*sin(pcoord->x1v(i)/L)*cos(pcoord->x2v(j)/L)*cos(pcoord->x3v(k)/L); //x momentum, L = 2pi
+    phydro->u(IM1,k,j,i) = rho*v0*sin(i/L)*cos(j/L)*cos(k/L); //x momentum
+    phydro->u(IM2,k,j,i) = -1*rho*v0*cos(i/L)*sin(j/L)*cos(k/L); //y momentum
     phydro->u(IM3,k,j,i) = 0.0; //z momentum
       
     if (NON_BAROTROPIC_EOS) {
-        phydro->u(IEN,k,j,i) = 0.5*rho*((phydro->u(IM1,k,j,i)/rho)*(phydro->u(IM1,k,j,i)/rho) + (phydro->u(IM2,k,j,i)/rho)*(phydro->u(IM2,k,j,i)/rho) + (phydro->u(IM3,k,j,i)/rho)*(phydro->u(IM3,k,j,i)/rho)) + rho*p/(rho*(gamma-1.0));
+        phydro->u(IEN,k,j,i) = 0.5*rho*
+        ((i/rho)*(i/rho) +
+         (j/rho)*(j/rho) +
+         (k/rho)*(k/rho)) +
+        rho*p/(rho*(gamma-1.0));
         }
     }}}
     
